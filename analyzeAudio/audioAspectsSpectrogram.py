8993d1b7d54434466ae9bd44dda76f03266b33b1
Hunter Hogan <hunterhogan@users.noreply.github.com>
2024-09-21 19:15:03 -0500

    numpy.ndarray

--- analyzeAudio/audioAspectsSpectrogram.py
+++ analyzeAudio/audioAspectsSpectrogram.py
@@ -1,29 +1,28 @@
 from analyzeAudio import registrationAudioAspect, audioAspects
 from functools import cache
-from numpy.typing import NDArray
 from typing import Any
 import librosa
 import numpy
 
 @registrationAudioAspect('Chromagram')
-def analyzeChromagram(spectrogramPower: NDArray, sampleRate: int, **kwargs: Any) -> NDArray:
+def analyzeChromagram(spectrogramPower: numpy.ndarray, sampleRate: int, **kwargs: Any) -> numpy.ndarray:
     return librosa.feature.chroma_stft(S=spectrogramPower, sr=sampleRate, **kwargs)
 
 @registrationAudioAspect('Spectral Contrast')
-def analyzeSpectralContrast(spectrogramMagnitude: NDArray, **kwargs: Any) -> NDArray:
+def analyzeSpectralContrast(spectrogramMagnitude: numpy.ndarray, **kwargs: Any) -> numpy.ndarray:
     return librosa.feature.spectral_contrast(S=spectrogramMagnitude, **kwargs)
 
 @registrationAudioAspect('Spectral Bandwidth')
-def analyzeSpectralBandwidth(spectrogramMagnitude: NDArray, **kwargs: Any) -> NDArray:
+def analyzeSpectralBandwidth(spectrogramMagnitude: numpy.ndarray, **kwargs: Any) -> numpy.ndarray:
     centroid = audioAspects['Spectral Centroid']['analyzer'](spectrogramMagnitude)
     return librosa.feature.spectral_bandwidth(S=spectrogramMagnitude, centroid=centroid, **kwargs)
 
 @cache
 @registrationAudioAspect('Spectral Centroid')
-def analyzeSpectralCentroid(spectrogramMagnitude: NDArray, **kwargs: Any) -> NDArray:
+def analyzeSpectralCentroid(spectrogramMagnitude: numpy.ndarray, **kwargs: Any) -> numpy.ndarray:
     return librosa.feature.spectral_centroid(S=spectrogramMagnitude, **kwargs)
 
 @registrationAudioAspect('Spectral Flatness')
-def analyzeSpectralFlatness(spectrogramMagnitude: NDArray, **kwargs: Any) -> NDArray:
+def analyzeSpectralFlatness(spectrogramMagnitude: numpy.ndarray, **kwargs: Any) -> numpy.ndarray:
     spectralFlatness = librosa.feature.spectral_flatness(S=spectrogramMagnitude, **kwargs)
     return 20 * numpy.log10(spectralFlatness, where=spectralFlatness != 0) # dB
--- analyzeAudio/audioAspectsWaveform.py
+++ analyzeAudio/audioAspectsWaveform.py
@@ -1,26 +1,25 @@
 from analyzeAudio import audioAspects, registrationAudioAspect
 from functools import cache
-from numpy.typing import NDArray
 from typing import Any
 import librosa
 import numpy
 
 @cache
 @registrationAudioAspect('Tempogram')
-def analyzeTempogram(waveform: NDArray, sampleRate: int, **kwargs: Any) -> NDArray:
+def analyzeTempogram(waveform: numpy.ndarray, sampleRate: int, **kwargs: Any) -> numpy.ndarray:
     return librosa.feature.tempogram(y=waveform, sr=sampleRate, **kwargs)
 
 # "RMS value from audio samples is faster ... However, ... spectrogram ... more accurate ... because ... windowed"
 @registrationAudioAspect('RMS')
-def analyzeRMS(waveform: NDArray, **kwargs: Any) -> NDArray:
+def analyzeRMS(waveform: numpy.ndarray, **kwargs: Any) -> numpy.ndarray:
     arrayRMS = librosa.feature.rms(y=waveform, **kwargs)
     return 20 * numpy.log10(arrayRMS, where=arrayRMS != 0) # dB
 
 @registrationAudioAspect('Tempo')
-def analyzeTempo(waveform: NDArray, sampleRate: int, **kwargs: Any) -> NDArray:
+def analyzeTempo(waveform: numpy.ndarray, sampleRate: int, **kwargs: Any) -> numpy.ndarray:
     tempogram = audioAspects['Tempogram']['analyzer'](waveform, sampleRate)
     return librosa.feature.tempo(y=waveform, sr=sampleRate, tg=tempogram, **kwargs)
 
 @registrationAudioAspect('Zero-crossing rate') # This is distinct from 'Zero-crossings rate'
-def analyzeZeroCrossingRate(waveform: NDArray, **kwargs: Any) -> NDArray:
+def analyzeZeroCrossingRate(waveform: numpy.ndarray, **kwargs: Any) -> numpy.ndarray:
     return librosa.feature.zero_crossing_rate(y=waveform, **kwargs)
